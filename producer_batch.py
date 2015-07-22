f08d9dbd002f3f2a4dcf626a18ab8d8a7b0f5fd7
Obulpathi <obulapathi.challa@rackspace.com>
2015-07-22 16:39:10 -0400

    Producer batch

---
+++ producer_batch.py
@@ -0,0 +1,24 @@
+import logging
+from datetime import datetime
+
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+
+batch_size = 50
+batch_timeout = 60
+
+logging.basicConfig()
+
+endpoint = "10.190.240.235:6667"
+client =  KafkaClient(endpoint)
+
+producer = SimpleProducer(client,
+    batch_send_every_n=batch_size, batch_send_every_t=batch_timeout)
+
+msg = """This is message sent from python client fgskjfh fskdjfh skdjfkjs dfhksdf kjfskjdfsjkd kjfsjdkfsdfd
+    sk kfjshkjdf jkjksfdfkjsd kjfsdhkjf sdhjkfsdkj ckjhdfk kjasfakfa kfakfhaskfasu kjfadkfj  aklfkasdf"""
+
+print "Start time: " + str(datetime.now().time())
+for i in range(5000000):
+    producer.send_messages("logs", msg)
+print "End time: " + str(datetime.now().time())
