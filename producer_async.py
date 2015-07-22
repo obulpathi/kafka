970d42995e76f4db30c203849f214acfbbe3d49c
Obulpathi <obulapathi.challa@rackspace.com>
2015-07-22 16:53:37 -0400

    Producer async

---
+++ producer_async.py
@@ -0,0 +1,21 @@
+import logging
+from datetime import datetime
+
+from kafka.client import KafkaClient
+from kafka.producer import SimpleProducer
+
+
+logging.basicConfig()
+
+endpoint = "10.190.240.235:6667"
+client =  KafkaClient(endpoint)
+
+producer = SimpleProducer(client, async=True)
+
+msg = """This is message sent from python client fgskjfh fskdjfh skdjfkjs dfhksdf kjfskjdfsjkd kjfsjdkfsdfd
+    sk kfjshkjdf jkjksfdfkjsd kjfsdhkjf sdhjkfsdkj ckjhdfk kjasfakfa kfakfhaskfasu kjfadkfj  aklfkasdf"""
+
+print "Start time: " + str(datetime.now().time())
+for i in range(5000000):
+    producer.send_messages("logs", msg)
+print "End time: " + str(datetime.now().time())
